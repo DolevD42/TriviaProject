2ea31fe480752e8e74649844d4cdb50a96c5b27f
DolevD42 <dolevdilmoney@gmail.com>
2022-04-08 14:56:30 +0300

    Build Communicator
    add the communicator and the first connection with the client

---
+++ TriviaProject/Communicator.cpp
@@ -0,0 +1,87 @@
+#include "Communicator.h"
+#define PORT 8876
+Communicator::Communicator()
+{
+
+	// this server use TCP. that why SOCK_STREAM & IPPROTO_TCP
+	// if the server use UDP we will use: SOCK_DGRAM & IPPROTO_UDP
+	m_serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (m_serverSocket == INVALID_SOCKET)
+		throw std::exception(__FUNCTION__ " - socket");
+}
+
+Communicator::~Communicator()
+{
+	try
+	{
+		// the only use of the destructor should be for freeing 
+		// resources that was allocated in the constructor
+		closesocket(m_serverSocket);
+	}
+	catch (...) {}
+}
+
+void Communicator::startHandleRequests()
+{
+	struct sockaddr_in sa = { 0 };
+
+	sa.sin_port = htons(PORT); // port that server will listen for
+	sa.sin_family = AF_INET;   // must be AF_INET
+	sa.sin_addr.s_addr = INADDR_ANY;    // when there are few ip's for the machine. We will use always "INADDR_ANY"
+
+	// Connects between the socket and the configuration (port and etc..)
+	if (bind(m_serverSocket, (struct sockaddr*)&sa, sizeof(sa)) == SOCKET_ERROR)
+		throw std::exception(__FUNCTION__ " - bind");
+
+	// Start listening for incoming requests of clients
+	if (listen(m_serverSocket, SOMAXCONN) == SOCKET_ERROR)
+		throw std::exception(__FUNCTION__ " - listen");
+	std::cout << "Listening on port " << PORT << std::endl;
+
+	while (true)
+	{
+		// the main thread is only accepting clients 
+		// and add then to the list of handlers
+		std::cout << "Waiting for client connection request" << std::endl;
+		bindAndListen();
+	}
+}
+
+void Communicator::bindAndListen()
+{
+	// this accepts the client and create a specific socket from server to this client
+	// the process will not continue until a client connects to the server
+	SOCKET client_socket = accept(m_serverSocket, NULL, NULL);
+	if (client_socket == INVALID_SOCKET)
+		throw std::exception(__FUNCTION__);
+
+	std::cout << "Client accepted. Server and client can speak" << std::endl;
+	_threadVector.push_back(std::thread(&Communicator::handleNewClient, this, client_socket));
+	auto it = _threadVector.rbegin();
+	it->detach();
+
+
+	// the function that handle the conversation with the client
+}
+
+void Communicator::handleNewClient(SOCKET clientSocket)
+{
+	std::pair< SOCKET, IRequestHandler* > pair;
+	pair.first = clientSocket;
+	LoginRequestHandler* log = new LoginRequestHandler();
+	pair.second = log;
+	m_clients.insert(pair);
+	try
+	{
+		Helper::sendData(clientSocket, "Hello");
+
+		std::string msg = Helper::getStringPartFromSocket(clientSocket, 5);
+		std::cout << msg << std::endl;
+	}
+	catch (const std::exception&)
+	{
+
+	}
+	
+}
---
+++ TriviaProject/Communicator.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <ostream>
+#include <WinSock2.h>
+#include <Windows.h>
+#include <vector>
+#include <thread>
+#include <map>
+#include "LoginRequestHandler.h"
+#include "Helper.h"
+
+class Communicator
+{
+public:
+	Communicator();
+	~Communicator();
+	void startHandleRequests();
+
+private:
+	SOCKET m_serverSocket;
+	std::vector<std::thread> _threadVector;
+	std::map<SOCKET, IRequestHandler*> m_clients;
+	void bindAndListen();
+
+	void handleNewClient(SOCKET clientSocket);
+};
+
