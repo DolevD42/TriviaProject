88b4b0808c20052557630e9dad777f0c4c97f3a6
Rico9939 <dinosaurgaming35@gmail.com>
2022-04-13 12:43:38 +0300

    updated the look of the code

--- Client.py
+++ Client.py
@@ -1,15 +1,24 @@
-
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 8876  # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as Csock:
-    try:
-        Csock.connect((HOST, PORT))
-        data = Csock.recv(5).decode()
-        print(data)
-        if data == "Hello":
-            Csock.sendall(b"Hello")
-    except Exception as e:
-        print(e)
+
+
+def main():
+    runtime = 0
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as Csock:
+        try:
+            Csock.connect((HOST, PORT))
+            if runtime == 0:
+                data = Csock.recv(5).decode()
+                print(data)
+                if data == "Hello":
+                    Csock.sendall(b"Hello")
+                    runtime = 1
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    main()
