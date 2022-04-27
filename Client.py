673723c68b795b8bc6b4f49d1d95a6064e1d6b60
Rico9939 <dinosaurgaming35@gmail.com>
2022-04-27 18:33:54 +0300

    added json sending and reciveing in the client

--- Client.py
+++ Client.py
@@ -1,8 +1,10 @@
 import socket
+import json
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 8876  # The port used by the server
-
+LOGIN = {"username": "user1", "password": 1234}
+SIGN_IN = {"username": "user1", "password": 1234, "mail": "user1@gmail.com"}
 
 
 def main():
@@ -16,6 +18,12 @@ def main():
                 if data == "Hello":
                     Csock.sendall(b"Hello")
                     runtime = 1
+            data = Csock.recv(1024).decode()
+            print(data)
+            data = json.loads(data)
+            if data.status == 1:
+                messageToSend = json.dumps(LOGIN)
+                Csock.sendall(messageToSend.encode())
         except Exception as e:
             print(e)
 
