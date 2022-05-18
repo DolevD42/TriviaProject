36ebd88ab640e29589b098b2f02f1800e30436e3
DolevD42 <dolevdilmoney@gmail.com>
2022-05-18 23:12:07 +0300

    Add ""
    Add "" to SqliteDataBase

--- TriviaProject/SqliteDataBase.cpp
+++ TriviaProject/SqliteDataBase.cpp
@@ -9,7 +9,7 @@ bool SqliteDataBase::open()
 {
 	if (_db == nullptr)
 	{
-		if (sqlite3_open("GalleryDB.sqlite", &_db) != SQLITE_OK)
+		if (sqlite3_open("TriviaDB.sqlite", &_db) != SQLITE_OK)
 		{
 			_db = nullptr;
 			throw std::exception("DB don't exist");
@@ -21,21 +21,21 @@ bool SqliteDataBase::open()
 bool SqliteDataBase::doesUserExist(std::string userName)
 {
 	open();
-	bool flag = false;
-	std::string msg = "select * from users where username == " + userName + ";";
-	if (sqlite3_exec(_db, msg.c_str(), exists, &flag, &_errMessage) != SQLITE_OK)
+	bool retunValue = false;
+	std::string msg = "select * from users where username == '" + userName + "';";
+	if (sqlite3_exec(_db, msg.c_str(), exists, &retunValue, &_errMessage) != SQLITE_OK)
 	{
-		throw std::exception("DB don't exist");
+		std::cout << "Falied to open DB" << std::endl;
 	}
+	return retunValue;
 
-	return flag;
 }
 
 bool SqliteDataBase::doesPasswordMatch(std::string userName, std::string pass)
 {
 	open();
 	bool flag = false;
-	std::string msg = "select * from users where username == " + userName + " and password = '" + pass + "';";
+	std::string msg = "select * from users where username == '" + userName + "' and password = '" + pass + "';";
 	if (sqlite3_exec(_db, msg.c_str(), exists, &flag, &_errMessage) != SQLITE_OK)
 	{
 		throw std::exception("DB don't exist");
@@ -47,7 +47,7 @@ bool SqliteDataBase::doesPasswordMatch(std::string userName, std::string pass)
 void SqliteDataBase::addNewUser(std::string userName, std::string pass, std::string email)
 {
 	open();
-	std::string msg = "INSERT INTO users (username, password, email) VALUES('" + userName + "', '" + pass + "', " + email + ");";
+	std::string msg = "INSERT INTO users (username, password, email) VALUES('" + userName + "', '" + pass + "', '" + email + "');";
 	if (sqlite3_exec(_db, msg.c_str(), nullptr, nullptr, &_errMessage) != SQLITE_OK)
 	{
 		throw std::exception("DB don't exist");
