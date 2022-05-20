a8f22417df6d03bed41e2c8a9981873d97944f70
DolevD42 <dolevdilmoney@gmail.com>
2022-05-20 11:54:35 +0300

    Build Serializer
    Create the first defention of RoomSerializer

--- TriviaProject/JsonResponsePacketSerializer.h
+++ TriviaProject/JsonResponsePacketSerializer.h
@@ -16,6 +16,14 @@ using nlohmann::json;
 #define ERR_CODE 41
 #define LOGIN_CODE 42
 #define SIGNUP_CODE 43
+#define LOGOUT_CODE 44
+#define GET_ROOMS_CODE 45
+#define GET_PLAYERS_CODE 46
+#define GET_HIGH_CODE 47
+#define GET_PERSONAL_CODE 48
+#define JOIN_ROOM_CODE 49
+#define CREATE_ROOM_CODE 50
+
 typedef struct LoginResponse {
 	unsigned int status;
 } LoginResponse;
@@ -23,6 +31,39 @@ typedef struct LoginResponse {
 typedef struct SignupResponse {
 	unsigned int status;
 } SignupResponse;
+
+typedef struct LogoutResponse {
+	unsigned int status;
+} LogoutResponse;
+
+typedef struct GetRoomsResponse {
+	unsigned int status;
+	std::vector<RoomData> rooms;
+} GetRoomsResponse;
+
+typedef struct GetPlayersInRoomResponse {
+	std::vector<std::string> players;
+} GetPlayersInRoomResponse;
+
+typedef struct GetHighScoreResponse {
+	unsigned int status;
+	std::vector<std::string> statistics;
+} GetHighScoreResponse;
+
+typedef struct GetPersonalStatsResponse {
+	unsigned int status;
+	std::vector<std::string> statistics;
+} GetPersonalStatsResponse;
+
+typedef struct JoinRoomResponse {
+	unsigned int status;
+} JoinRoomResponse;
+
+typedef struct CreateRoomResponse {
+	unsigned int status;
+} CreateRoomResponse;
+
+
 
 typedef struct ErrorResponse {
 	std::string message;
@@ -33,6 +74,13 @@ public:
 	static std::vector<char> serializeResponse(ErrorResponse msg);
 	static std::vector<char> serializeResponse(LoginResponse msg);
 	static std::vector<char> serializeResponse(SignupResponse msg);
+	static std::vector<char> serializeResponse(LogoutResponse msg);
+	static std::vector<char> serializeResponse(GetRoomsResponse msg);
+	static std::vector<char> serializeResponse(GetPlayersInRoomResponse msg);
+	static std::vector<char> serializeResponse(JoinRoomResponse msg);
+	static std::vector<char> serializeResponse(CreateRoomResponse msg);
+	static std::vector<char> serializeResponse(GetHighScoreResponse msg);
+	static std::vector<char> serializeResponse(GetPersonalStatsResponse msg);
 private:
 	static std::vector<char> onlyStatus(int code, int len, std::string info);
 };
