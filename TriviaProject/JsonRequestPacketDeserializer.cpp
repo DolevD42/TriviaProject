06d3e5f1e69bc2be88741688f1723d4487fc27cc
Rico9939 <dinosaurgaming35@gmail.com>
2022-05-21 11:21:03 +0300

    addded all deserilize funcitons

--- TriviaProject/JsonRequestPacketDeserializer.cpp
+++ TriviaProject/JsonRequestPacketDeserializer.cpp
@@ -27,15 +27,83 @@ SignupRequest JsonRequestPacketDeserializer::deserializeSignupRequest(std::vecto
 
 GetPlayersInRoomRequest JsonRequestPacketDeserializer::deserializeGetPlayersRequest(const aVec& buffer)
 {
-    return GetPlayersInRoomRequest();
+    GetPlayersInRoomRequest request;
+    std::string data = JsonRequestPacketDeserializer::extractData(buffer);
+    nlohmann::json jsonObj = nlohmann::json::parse(data);
+    unsigned int tempStr = jsonObj.value("roomId", 0);
+    request.roomId = tempStr;
+    return request;
 }
 
 JoinRoomRequest JsonRequestPacketDeserializer::deserializeJoinRoomRequest(const aVec& buffer)
 {
-    return JoinRoomRequest();
+    JoinRoomRequest request;
+    std::string data = JsonRequestPacketDeserializer::extractData(buffer);
+    nlohmann::json jsonObj = nlohmann::json::parse(data);
+    unsigned int tempStr = jsonObj.value("roomId", 0);
+    request.roomId = tempStr;
+    return request;
 }
 
 CreateRoomRequest JsonRequestPacketDeserializer::deserializeCreateRoomReuquest(const aVec& buffer)
 {
-    return CreateRoomRequest();
+    CreateRoomRequest request;
+    std::string data = JsonRequestPacketDeserializer::extractData(buffer);
+    nlohmann::json jsonObj = nlohmann::json::parse(data);
+    std::string tempStr = std::string(jsonObj.value("roomName", "unknown"));
+    request.roomName = tempStr;
+    unsigned int tempNum = jsonObj.value("maxUsers", 0);
+    request.maxUsers = tempNum;
+    tempNum = jsonObj.value("questionCount", 0);
+    request.questionCount = tempNum;
+    tempNum = jsonObj.value("answerTimeout", 0);
+    request.answerTimeout = tempNum;
+    tempNum = jsonObj.value("questionCount", 0);
+    request.questionCount = tempNum;
+    return request;
+
+}
+
+std::string JsonRequestPacketDeserializer::extractData(const aVec& buffer)
+{
+    unsigned int len = 0;
+    aVec sliced = slice(buffer, CODE_END, DATALEN_LEN);
+    std::string toRet = "";
+    memcpy(&len, sliced.data(), DATALEN_LEN);
+    sliced = slice(buffer, CODE_END + DATALEN_LEN, len);
+    for (unsigned int i = 0; i < len; i++) {
+        toRet += (char)sliced[i].to_ulong();
+    }
+    return toRet;
+
+}
+
+SubmitAnswerRequest JsonRequestPacketDeserializer::deserializeSubmitAnswerRequest(const aVec& buffer)
+{
+    SubmitAnswerRequest request;
+    std::string data = JsonRequestPacketDeserializer::extractData(buffer);
+    nlohmann::json jsonObj = nlohmann::json::parse(data);
+    unsigned int tempStr = jsonObj.value("answerId", 0);
+    request.answerId = tempStr;
+    return request;
+
+}
+
+void printPtr(const aVec& ptr, const unsigned int len)
+{
+    printf("\n");
+    for (unsigned int i = 0; i < len; i++)
+    {
+        printf("%u ", (unsigned char)ptr[i].to_ulong());
+    }
+}
+
+aVec slice(const aVec& str, const unsigned int start, const unsigned int len)
+{
+    aVec sliced(len);
+    for (unsigned int i = 0; i < len; i++) {
+        sliced[i] = (char)str[i + start].to_ulong();
+    }
+    return sliced;
+
 }
--- TriviaProject/JsonRequestPacketDeserializer.h
+++ TriviaProject/JsonRequestPacketDeserializer.h
@@ -4,6 +4,8 @@
 #include "Helper.h"
 #include "json.hpp"
 #include <bitset>
+#define CODE_END 1
+#define DATALEN_LEN 4
 using nlohmann::json;
 typedef std::vector<std::bitset<8>> aVec;
 typedef struct LoginRequest {
@@ -17,12 +19,33 @@ typedef struct SignupRequest {
 	std::string email;
 } SignupRequest;
 
+typedef struct GetPlayersInRoomRequest {
+	unsigned int roomId;
+} GetPlayersInRoomRequest;
+typedef struct JoinRoomRequest {
+	unsigned int roomId;
+} JoinRoomRequest;
+typedef struct CreateRoomRequest {
+	std::string roomName;
+	unsigned int maxUsers;
+	unsigned int questionCount;
+	unsigned int answerTimeout;
+} CreateRoomRequest;
+typedef struct SubmitAnswerRequest {
+	unsigned int answerId;
+} SubmitAnswerRequest;
+
+
 class JsonRequestPacketDeserializer {
+	static std::string extractData(const aVec& data);
 public:
 	static LoginRequest deserializeLoginRequest(std::vector<char> buffer);
 	static SignupRequest deserializeSignupRequest(std::vector<char> buffer);
 	static GetPlayersInRoomRequest deserializeGetPlayersRequest(const aVec& buffer);
 	static JoinRoomRequest deserializeJoinRoomRequest(const aVec& buffer);
 	static CreateRoomRequest deserializeCreateRoomReuquest(const aVec& buffer);
+	static SubmitAnswerRequest deserializeSubmitAnswerRequest(const aVec& buffer);
 
 };
+void printPtr(const aVec& ptr, const unsigned int len);
+aVec slice(const aVec& str, const unsigned int start, const unsigned int len);
