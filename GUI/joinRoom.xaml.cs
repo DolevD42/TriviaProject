aa1fd787bdef36e59af01685c8337b6f64b887e4
Rico9939 <dinosaurgaming35@gmail.com>
2022-06-08 20:09:38 +0300

    added threads finishing tommaro

--- GUI/joinRoom.xaml.cs
+++ GUI/joinRoom.xaml.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Threading;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 using System.Windows.Shapes;
 using System.Net.Sockets;
 using System.Net;
@@ -33,6 +35,8 @@ namespace GUI
             JoinButton.IsEnabled = false;
             string msgToSent = Serializer.serializeCodeOnly(Consts.GET_ROOMS_CODE);
             NetworkStream net = _client.GetStream();
+            Thread RefresherThread = new Thread(() => refresh(net));
+            RefresherThread.Start();
             net.Write(System.Text.Encoding.ASCII.GetBytes(msgToSent), 0, msgToSent.Length);
             byte[] serverMsg = new byte[5];
             net.Read(serverMsg, 0, 5);
@@ -46,7 +50,7 @@ namespace GUI
                 return;
             }
             byte[] serverBuffer = new byte[resInf.len];
-
+            
             net.Read(serverBuffer, 0, resInf.len);
             Consts.GetRoomsResponse res = Deserializer.deserializeGetRoomsResponse(Encoding.Default.GetString(serverBuffer));
             for (int i = 0; i < res.roomsId.Count(); i++)
@@ -55,8 +59,42 @@ namespace GUI
                 _roomsId.Add(res.roomsId[i]);
                 _roomNames.Add(res.rooms[i]);
             }
-            
+
         }
+
+        private void refresh(NetworkStream rnet)
+        {
+            List<int> _roomsIdR = new List<int>();
+            List<string> _roomNamesR = new List<string>();
+            System.Threading.Thread.Sleep(3000);
+            while (true)
+            {
+                string msgToSent = Serializer.serializeCodeOnly(Consts.GET_ROOMS_CODE);
+                rnet.Write(System.Text.Encoding.ASCII.GetBytes(msgToSent), 0, msgToSent.Length);
+                byte[] serverMsg = new byte[5];
+                rnet.Read(serverMsg, 0, 5);
+                Consts.ResponseInfo resInf = Deserializer.deserializeSize(Encoding.Default.GetString(serverMsg));
+                if (resInf.id == Consts.ERR_CODE)
+                {
+                    byte[] errorBuffer = new byte[resInf.len];
+                    rnet.Read(errorBuffer, 0, resInf.len);
+                    Consts.ErrorResponse err = Deserializer.deserializeErrorResponse(Encoding.Default.GetString(errorBuffer));
+                    MessageBox.Show(err.msg, "Trivia Client", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                byte[] serverBuffer = new byte[resInf.len];
+
+                rnet.Read(serverBuffer, 0, resInf.len);
+                Consts.GetRoomsResponse res = Deserializer.deserializeGetRoomsResponse(Encoding.Default.GetString(serverBuffer));
+                for (int i = 0; i < res.roomsId.Count(); i++)
+                {
+                    //list.Items.Add(res.roomsId[i] + ":      " + res.rooms[i]);
+                    _roomsIdR.Add(res.roomsId[i]);
+                    _roomNamesR.Add(res.rooms[i]);
+                }//add new list and 
+            }
+        }
+
         private void ListSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             JoinButton.IsEnabled = true;
@@ -82,19 +120,6 @@ namespace GUI
             byte[] serverBuffer = new byte[resInf.len];
 
             net.Read(serverBuffer, 0, resInf.len);
-            Consts.StatusResponse res = Deserializer.deserializeJoinRoomResponse(Encoding.Default.GetString(serverBuffer));
-            switch (res.status)
-            {
-                case Consts.ROOM_MAX_OUT:
-                    MessageBox.Show("There is no place left in the room", "Trivia Client", MessageBoxButton.OK, MessageBoxImage.Exclamation);
-                    break;
-                case Consts.REQUEST_VALID:
-                    this.Hide();
-                    RoomMember win = new RoomMember(_client, _username, _roomsId[list.SelectedIndex], _roomNames[list.SelectedIndex]);
-                    win.Show();
-                    this.Close();
-                    break;
-            }
         }
         private void BackToMenuClick(object sender, RoutedEventArgs e)
         {
@@ -103,5 +128,6 @@ namespace GUI
             win.Show();
             this.Close();
         }
+
     }
 }
