c54ddb143ca9a79a700fd1aa63529c0816d5b400
Rico9939 <dinosaurgaming35@gmail.com>
2022-06-13 17:57:26 +0300

    game header

---
+++ TriviaProject/Game.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "SqliteDataBase.h"
+#include "IDataBase.h"
+#include "LoggedUser.h"
+#include "GameData.h"
+#include <map>
+class GameManager
+{
+private:
+	std::vector<Question> m_questions;
+	std::map<LoggedUser, GameData> m_players;
+	int m_gameId;
+public:
+	Question getQuestionForUser(LoggedUser users);
+	void submitAnswer(LoggedUser users,int answeriD);
+	void removePlayer(LoggedUser users);
+};
