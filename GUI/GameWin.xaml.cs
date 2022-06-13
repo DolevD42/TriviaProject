93f11a84ebc1ff3e80f4537379de631eefb2e4e1
Rico9939 <dinosaurgaming35@gmail.com>
2022-06-13 18:12:01 +0300

    added timer to game window

--- GUI/GameWin.xaml.cs
+++ GUI/GameWin.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,14 +21,43 @@ namespace GUI
     /// </summary>
     public partial class GameWin : Window
     {
+        private Thread Timer;
         private TcpClient _client;
         private string _username;
-
+        private NetworkStream _net;
         public GameWin(TcpClient client, string username)
         {
             InitializeComponent();
             this._client = client;
             this._username = username;
+            NetworkStream net = _client.GetStream();
+            _net = net;
+            string msgToSent = Serializer.serializeCodeOnly(Consts.GET_ROOM_STATE_CODE);
+            
+        }
+        public delegate void TickCallback();
+        public delegate int ClockCallback();
+        /// <summary>
+        /// The Main Function for the thread.
+        /// </summary>
+        private void Tick()
+        {
+            while (!Clock.Dispatcher.Invoke(new ClockCallback(ClockTime)).Equals(0))
+            {
+                Thread.Sleep(1000);
+                Clock.Dispatcher.Invoke(new TickCallback(Timer_Tick));
+            }
+
+        }
+
+        void Timer_Tick()
+        {
+            Clock.Text = (Convert.ToInt16(Clock.Text) - 1).ToString();
+        }
+
+        int ClockTime()
+        {
+            return Convert.ToInt32(Clock.Text);
         }
     }
 }
