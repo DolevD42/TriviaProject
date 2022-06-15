bcef81b7e0ea16d7e1ac5aa73113965574d6cbc0
Rico9939 <dinosaurgaming35@gmail.com>
2022-06-15 15:43:27 +0300

    created more functions

--- TriviaProject/GameManager.h
+++ TriviaProject/GameManager.h
@@ -3,12 +3,14 @@
 #include "IDataBase.h"
 #include "Game.h"
 #include "LoggedUser.h"
+#include "Room.h"
 class GameManager
 {
 private:
 	IDataBase* m_database;
 	std::vector<Game> m_games;
+	int _gameId;
 public:
-	Game CreateGame(std::string userName);
-	void deleteGame();
+	Game CreateGame(Room room);
+	void deleteGame(int gameId);
 };
