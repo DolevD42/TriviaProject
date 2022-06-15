74afa365400dc8b0e3bb46707414c6b333871594
Rico9939 <dinosaurgaming35@gmail.com>
2022-06-15 15:43:02 +0300

    added header internal functions

--- TriviaProject/Game.h
+++ TriviaProject/Game.h
@@ -4,21 +4,23 @@
 #include "LoggedUser.h"
 #include "Question.h"
 #include <map>
-typedef struct GameData {
-	Question currentQuestion;
+
+struct GameData {
+	Question* currentQuestion;
 	unsigned int CorrectAnswerCount;
 	unsigned int WrongAnswerCount;
 	float averageAnswerTime;
-} GameData;
-
+};
 class Game
 {
 private:
 	std::vector<Question*> m_questions;
-	std::map<LoggedUser, GameData> m_players;
+	std::map<LoggedUser*, GameData> m_players;
 	int m_gameId;
 public:
-	Question getQuestionForUser(LoggedUser users);
-	void submitAnswer(LoggedUser users,int answeriD);
-	void removePlayer(LoggedUser users);
+	Game(LoggedUser* User, unsigned int CorrectAnswerCount, unsigned int WrongAnswerCount, float averageAnswerTime);
+	Question* getQuestionForUser(LoggedUser* users);
+	void submitAnswer(LoggedUser* users,int answeriD);
+	void removePlayer(LoggedUser* users);
+	int getGameId();
 };
