5bfa4956e73b97bb18dd0c3bc6bfa363e9e5b879
Rico9939 <dinosaurgaming35@gmail.com>
2022-06-15 19:16:25 +0300

    fixed idatabase

--- TriviaProject/IDataBase.h
+++ TriviaProject/IDataBase.h
@@ -9,7 +9,7 @@ public:
 	virtual bool doesUserExist(std::string userName) = 0;
 	virtual bool doesPasswordMatch(std::string userName, std::string pass) = 0;
 	virtual void addNewUser(std::string userName, std::string pass, std::string email) = 0;
-	virtual std::list<Question*> getQuestions(int id) = 0;
+	virtual std::list<Question*> getQuestions() = 0;
 	virtual float getPlayerAverageAnswerTime(std::string id) = 0;
 	virtual int  getNumOfCorrectAnswers(std::string id) = 0;
 	virtual int  getNumOfTotalAnswers(std::string id) = 0;
