888d66f3f5c17b63d9a0ce3774220524a1a670c9
Rico9939 <dinosaurgaming35@gmail.com>
2022-06-15 15:43:20 +0300

    switched game manager to be more effishent

--- TriviaProject/GameManager.cpp
+++ TriviaProject/GameManager.cpp
@@ -1,10 +1,39 @@
 #include "GameManager.h"
-Game GameManager::CreateGame(std::string userName)
+Game GameManager::CreateGame(Room room)
 {
-	return;
+	try
+	{
+		_gameId = m_database->insertNewGame();
+		if (_gameId == -1)
+		{
+			throw std::exception("Couldn't insert game into the database");
+		}
+		RoomData req = room.getRoomData();
+		LoggedUser* user = room.getLoggedUser(req.id);
+		std::string userAc =user->getUsername();
+		int acceptedCount = m_database->getNumOfCorrectAnswers(userAc);
+		int wrongCount =m_database->getNumOfTotalAnswers(userAc) - m_database->getNumOfCorrectAnswers(userAc);
+		float timePer = m_database->getPlayerAverageAnswerTime(userAc);
+		Game newGame = Game(user, acceptedCount, wrongCount, timePer);
+		m_games.push_back(newGame);
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 }
 
-void GameManager::deleteGame()
+void GameManager::deleteGame(int gameId)
 {
-	return;
+	m_database->RemoveNewGame(gameId);
+	int curGameId;
+	for (std::vector<Game>::iterator it = m_games.end();it != m_games.begin();) 
+	{
+		curGameId = it->getGameId();
+		if (curGameId = gameId)
+		{
+			it = m_games.erase(it);
+			break;
+		}
+	}
 }
